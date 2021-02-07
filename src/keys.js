feeca42d93e6dac327f2f04f85785ec643eca1c6
rlivon <raullivon@gmail.com>
2021-02-07 00:55:16 -0300

    Actualización de conexión

--- src/keys.js
+++ src/keys.js
@@ -1,12 +1,19 @@
-
 module.exports = {
 
-    database: {
-        host:'localhost',
-        user: 'root',
-        password: 'samsung', // u~e)^|^9fyM&=f(-
-        database: 'database_link',
+        // database: {
+        //     host:'localhost',
+        //     user: 'root',
+        //     password: 'samsung',
+        //     database: 'database_link',
+    
+        // }
 
-    }
-
-};
+        database: {
+                host:'db4free.net',
+                user: 'rl_root',
+                password: '#Sofia21',
+                database: 'database_link',
+        
+            }
+    
+    };
